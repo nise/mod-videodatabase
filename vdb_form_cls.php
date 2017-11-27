024e3917879bfa95b87106897852871bfa6ebbc4
nise <niels.seidel@nise81.com>
2017-11-27 11:10:48 +0100

    bam

---
+++ vdb_form_cls.php
@@ -0,0 +1,20 @@
+//moodleform is defined in formslib.php
+require_once("$CFG->libdir/formslib.php");
+ 
+class simplehtml_form extends moodleform {
+    //Add elements to form
+    public function definition() {
+        global $CFG;
+ 
+        $mform = $this->_form; // Don't forget the underscore! 
+ 
+        $mform->addElement('text', 'email', get_string('email')); // Add elements to your form
+        $mform->setType('email', PARAM_NOTAGS);                   //Set type of element
+        $mform->setDefault('email', 'Please enter email');        //Default value
+            ...
+    }
+    //Custom validation should be added here
+    function validation($data, $files) {
+        return array();
+    }
+}
